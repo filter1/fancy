bca2197a5275e05df0c1499adcbe3d460ccfe8d1
Johannes Filter <filter@johanness-mbp.d-lsi.uned.es>
2015-06-03 15:09:29 +0200

    fix sass gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,9 @@
 var gulp = require('gulp'),
-  // connect = require('gulp-connect'),
   watch = require('gulp-watch'),
   coffee = require('gulp-coffee'),
   sass = require('gulp-sass'),
   uglify  = require('gulp-uglify'),
   concat  = require('gulp-concat');
- 
-// gulp.task('webserver', function() {
-//   connect.server({
-//     livereload: true,
-//     root: 'app'
-//   });
-// });
- 
-// gulp.task('livereload', function() {
-//   watch('app/*')
-//     .pipe(connect.reload());
-// });
  
 gulp.task('coffee-public', function() {
   gulp.src('app/public/scripts/*.coffee')
@@ -32,8 +19,8 @@ gulp.task('coffee-app', function() {
     // .pipe(connect.reload());
 });
 
-
 gulp.task('sass', function() {
+  console.log('lala');
   gulp.src('app/public/styles/*.scss')
     .pipe(sass({errLogToConsole: true}))
     .pipe(gulp.dest('app/public/styles'));
@@ -42,9 +29,9 @@ gulp.task('sass', function() {
 
 // can add more here
 gulp.task('watch', function() {
-  gulp.watch('app/public/scripts/*.coffee', ['coffee-public'])
-  gulp.watch('app/*.coffee', ['coffee-app'])
-  gulp.watch('app/styles/*.scss', ['sass'])
+  gulp.watch('app/public/scripts/*.coffee', ['coffee-public']);
+  gulp.watch('app/*.coffee', ['coffee-app']);
+  gulp.watch('app/public/styles/*.scss', ['sass']);
 });
 
 
